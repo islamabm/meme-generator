b4a1e7ff809185b3da796948f69d51c56cd31782
islam abo mokh <alo.jalal.55@gmail.com>
2023-02-17 14:23:33 +0200

    css

--- js/utils.js
+++ js/utils.js
@@ -33,3 +33,40 @@ function getRandomFont() {
   var font = fonts[wordIdx]
   return font
 }
+
+function saveToStorage(key, val) {
+  localStorage.setItem(key, JSON.stringify(val))
+}
+
+function loadFromStorage(key) {
+  var val = localStorage.getItem(key)
+  return JSON.parse(val)
+}
+
+function setQueryParams(newParams) {
+  const url = new URL(window.location.href)
+  const params = new URLSearchParams(url.search)
+
+  for (var paramName in newParams) {
+    const paramValue = newParams[paramName]
+    params.set(paramName, paramValue) // used to update an existing query string parameter or add a new one if it doesn't exist.
+  }
+
+  url.search = params.toString()
+  window.history.pushState({ path: url.href }, '', url.href) //modify the URL of the current page without reloading the page
+}
+
+function deleteQueryParam(key) {
+  const url = new URL(window.location.href)
+  const params = new URLSearchParams(url.search)
+
+  params.delete(key)
+  url.search = params.toString()
+
+  window.history.pushState({ path: url.href }, '', url.href)
+}
+
+function getValFromParam(key) {
+  const queryStringParams = new URLSearchParams(window.location.search)
+  return queryStringParams.get(key)
+}
